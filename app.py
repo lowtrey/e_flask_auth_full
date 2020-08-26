cc704cdc4e24ea211a755701313865d55a3ac78c
Q. Price <priceqp@gmail.com>
2020-08-26 14:13:19 -0500

    create and test User model

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 
