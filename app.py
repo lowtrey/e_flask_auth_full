ca6a106f85be769ab04a63964dd781bef13ea5cc
Q. Price <priceqp@gmail.com>
2020-08-26 19:04:20 -0500

    implement user login and registration

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
+from forms import UserForm, LoginForm
 
 app = Flask(__name__)
 
@@ -17,4 +18,49 @@ connect_db(app)
 
 @app.route("/")
 def show_index():
-  return render_template("index.html")
+  return redirect("/register")
+
+# Secret Route
+@app.route("/secret")
+def show_secret():
+  return render_template("secret.html")
+
+# Registration Route
+@app.route("/register", methods=["GET", "POST"])
+def register_user():
+  form = UserForm()
+
+  if form.validate_on_submit():
+    username = form.username.data
+    password = form.password.data
+    email = form.email.data
+    first_name = form.first_name.data
+    last_name = form.last_name.data
+
+    # TODO: Add error handling for taken usernames and emails
+    new_user = User.register(username, password, email, first_name, last_name)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    flash("New User Created!", "success")
+    return redirect("/secret")
+  else:
+    return render_template("register.html", form=form)
+
+# Login Route
+@app.route("/login", methods=["GET", "POST"])
+def login_user():
+  form = LoginForm()
+
+  if form.validate_on_submit():
+    username = form.username.data
+    password = form.password.data
+    user = User.authenticate(username, password)
+    if user:
+      flash(f"Welcome Back, {user.username}!", "info")
+      return redirect("/secret")
+    else:
+      form.username.errors = ["Invalid username or password."]
+      
+  return render_template("login.html", form=form)
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def connect_db(app):
   db.app = app
@@ -20,3 +22,19 @@ class User(db.Model):
   first_name = db.Column(db.String(30), nullable=False)
 
   last_name = db.Column(db.String(30), nullable=False)
+
+  @classmethod
+  def register(cls, username, password, email, first_name, last_name):
+    hashed_password = bcrypt.generate_password_hash(password)
+    # Turn bytestring into normal (unicode utf8) string
+    hashed_password_utf8 = hashed_password.decode("utf8")
+    return cls(username=username, password=hashed_password_utf8, email=email, first_name=first_name, last_name=last_name)
+
+  @classmethod
+  def authenticate(cls, username, password):
+    user = User.query.filter_by(username=username).first()
+    valid_password = bcrypt.check_password_hash(user.password, password)
+    if user and valid_password:
+      return user
+    else:
+      return False
